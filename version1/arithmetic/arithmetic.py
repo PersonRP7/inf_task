4760ba1bf4a8ae82d5ff0c5f862fc0abff45fa75
Person <zemlja-trp@yandex.com>
2022-11-29 20:35:29 +0100

    Implemented inverter method / arithemtic.

--- version1/arithmetic/arithmetic.py
+++ version1/arithmetic/arithmetic.py
@@ -32,3 +32,16 @@ class Arithmetic:
         # root(4, 625)
         # return a**(1 / b)
         return int(b**(1 / a))
+
+    @staticmethod
+    def inverter(data:list)->list:
+        # data = [5, 3, 3]
+        """
+        Creates a cartesian product of all possible sign variations
+        for every digit in a three digit list comprising a given registration
+        number.
+        """
+        inverted_data = [-x for x in data]
+        res = list(product(*zip(data, inverted_data)))
+        # print(res)
+        return res
