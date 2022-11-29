15b4e0ddec973ac6fb08507c77caf34949407df4
Person <zemlja-trp@yandex.com>
2022-11-29 20:27:28 +0100

    Added root method.

--- version1/arithmetic/arithmetic.py
+++ version1/arithmetic/arithmetic.py
@@ -26,3 +26,9 @@ class Arithmetic:
     @staticmethod
     def power_to(a:int, b:int)->int:
         return int(a**b)
+
+    @staticmethod
+    def root(a:int, b:int)->int:
+        # root(4, 625)
+        # return a**(1 / b)
+        return int(b**(1 / a))
