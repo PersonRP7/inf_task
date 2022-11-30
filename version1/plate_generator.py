2806aa250fa3bed63cc8b963eeedcea9bf513b2d
Person <zemlja-trp@yandex.com>
2022-11-30 18:08:20 +0100

    Added docstrings for generate_plates and stringify_plates in plate_generator.py

--- version1/plate_generator.py
+++ version1/plate_generator.py
@@ -7,12 +7,20 @@ class PlateGenerator:
 
     @classmethod
     def generate_plates(cls)->None:
+        """
+        Plate number creation.
+        """
         cls.plates = random.sample(
             range(1000, 5000), 3000
         )
 
     @classmethod
     def stringify_plates(cls)->None:
+        """
+        Stringify plate numbers to make them iterable.
+        Iterability is needed for hyphen addition in the
+        penultimate position of a given plate number.
+        """
         for number in cls.plates:
             str(number)
 
